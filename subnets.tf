30c9e8f0ba659436a086ba8bb06c1c5eac5d26bb
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-08-14 14:03:44 +0200

    fix header for oradba

---
+++ subnets.tf
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# OraDBA - Oracle Database Infrastructur and Security, 5630 Muri, Switzerland
+# ------------------------------------------------------------------------------
+# Name.......: subnets.tf
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
+# Editor.....: Stefan Oehrli
+# Date.......: 2024.08.14
+# Revision...: 
+# Purpose....: Define subnets for the terraform module.
+# Notes......: -- 
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ------------------------------------------------------------------------------
+
+# create public subnet ---------------------------------------------------------
+resource "oci_core_subnet" "public_subnet" {
+  count             = var.internet_gateway_enabled == true ? var.numberOf_labs : 0
+  compartment_id    = var.compartment_id
+  cidr_block        = cidrsubnet(var.vcn_cidr, var.public_newbits, var.public_netnum)
+  display_name      = var.label_prefix == "none" ? format("${local.resource_shortname}%02d public subnet", count.index) : format("${var.label_prefix} ${local.resource_shortname}%02d public subnet", count.index)
+  dns_label         = local.public_dns_label
+  vcn_id            = oci_core_vcn.vcn[count.index].id
+  security_list_ids = [oci_core_vcn.vcn[count.index].default_security_list_id]
+  route_table_id    = oci_core_vcn.vcn[count.index].default_route_table_id
+  dhcp_options_id   = oci_core_vcn.vcn[count.index].default_dhcp_options_id
+}
+
+# create private subnet --------------------------------------------------------
+resource "oci_core_subnet" "private_subnet" {
+  count                      = var.nat_gateway_enabled == true ? var.numberOf_labs : 0
+  compartment_id             = var.compartment_id
+  cidr_block                 = cidrsubnet(var.vcn_cidr, var.private_newbits, var.private_netnum)
+  display_name               = var.label_prefix == "none" ? format("${local.resource_shortname}%02d private subnet", count.index) : format("${var.label_prefix} ${local.resource_shortname}%02d private subnet", count.index)
+  dns_label                  = local.private_dns_label
+  prohibit_public_ip_on_vnic = true
+  vcn_id                     = oci_core_vcn.vcn[count.index].id
+  security_list_ids          = [oci_core_vcn.vcn[count.index].default_security_list_id]
+  route_table_id             = oci_core_route_table.private_route_table[count.index].id
+  dhcp_options_id            = oci_core_dhcp_options.private_dhcp_option[count.index].id
+}
+# --- EOF ----------------------------------------------------------------------
