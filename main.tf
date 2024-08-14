bc135b877e7c5e9e458441d6a306eeb762928a2f
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-08-14 13:59:31 +0200

    fix header for oradba

---
+++ main.tf
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------------
+# OraDBA - Oracle Database Infrastructur and Security, 5630 Muri, Switzerland
+# ------------------------------------------------------------------------------
+# Name.......: main.tf
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@oradba.ch
+# Editor.....: Stefan Oehrli
+# Date.......: 2024.08.14
+# Revision...: 
+# Purpose....: Main file for the terraform module
+# Notes......: Define a minimal terraform version
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 1.0.0"
+}
+# --- EOF ----------------------------------------------------------------------
