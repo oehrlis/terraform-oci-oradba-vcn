05cd56ed83cfaafe130a55e0183ab1189fef25c4
Stefan Oehrli <stefan.oehrli@accenture.com>
2024-08-14 14:05:13 +0200

    fix header for oradba

---
+++ examples/main.tf
@@ -0,0 +1,35 @@
+# ---------------------------------------------------------------------------
+# Trivadis - Part of Accenture, Platform Factory - Data Platforms
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: main.tf
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@accenture.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2023.04.19
+# Revision...: 
+# Purpose....: Main file to use terraform module tvdlab vcn.
+# Notes......: -- 
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ---------------------------------------------------------------------------
+
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+# define the terraform provider
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+# --- EOF -------------------------------------------------------------------
